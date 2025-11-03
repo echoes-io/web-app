e977526f2943a0dc1ea66ac77915f5ebe1b12aa3
Zweer <n.olivieriachille@gmail.com>
2025-11-03 16:04:29 +0100

    fix: :ambulance: Fixing compiling errors

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,11 +1,10 @@
+import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from 'ws';
 
 import * as schema from '@/lib/db/models';
 
-export const db = drizzle({
-  connection: process.env.DATABASE_URL,
-  schema,
-  logger: true,
-  ws,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+pool.on('error', (err: Error) => console.error('Unexpected pool error', err));
+
+export const db = drizzle({ client: pool, schema, logger: true, ws });
